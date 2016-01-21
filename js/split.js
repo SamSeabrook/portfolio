ddc6ff32c286cea6a0029112dd8d183f54ec2e2d
Sam Seabrook <samseabrook@gmail.com>
2016-01-20 20:00:09 -0500

    added js navigation and scroll

--- js/split.js
+++ js/split.js
@@ -1,5 +1,5 @@
 jQuery(document).ready( function($){
-
+	/*-----Image Splitter javaScript-----*/
 	$('.coder-wrapper').on( "mouseenter", function() {
 		$(this).closest('.split-wrapper').find('.divider-bar').css({
 			left: '25%',
@@ -14,19 +14,31 @@ jQuery(document).ready( function($){
 			transition: 'all .3s'
 		});
 	});
-	$('.split-wrapper').on( "mouseleave", function() {
-		$(this).find('.divider-bar').css({
+	$('.hero-textwrapper').on( "mouseenter", function() {
+		$(this).closest('.split-wrapper').find('.divider-bar').css({
 			left: '75%',
 			transition: 'all .3s'
 		});
-		$(this).find('.coder-wrapper').css({
+		$(this).parent().find('.coder-wrapper').css({
 			transform: 'translateX(75%)',
 			transition: 'all .3s'
 		});
-		$(this).find('.coder-image').css({
+		$(this).parent().find('.coder-image').css({
 			transform: 'translateX(-75%)',
 			transition: 'all .3s'
 		});
 	});
 
+	/*-----Active Link-----*/
+	$('.page-nav ul li').click( function(){
+		$('.page-nav ul li.active').removeClass('active');
+		$(this).addClass('active');
+
+	});
+	/*-----Scroll Navigation-----*/
+	$('a').on('click', function() {
+		var location = (this).getAttribute("href");
+		$('html body').animate({'scrollTop':$(location).offset().top}, 500);
+		event.preventDefault();
+	});
 });
