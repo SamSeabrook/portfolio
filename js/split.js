05a2bf1d6c296c747c11e3bc780a2d10c8f795aa
Sam Seabrook <samseabrook@gmail.com>
2016-02-01 20:52:34 -0500

    Nav-bar fade in

--- js/split.js
+++ js/split.js
@@ -1,7 +1,8 @@
 jQuery(document).ready( function($){
-	$.ajaxSetup({ dataType: "jsonp" });
+	//$.ajaxSetup({ dataType: "jsonp" });
 
 	/*-----Image Splitter javaScript-----*/
+	// When cursor enters "right-side" container, slide divider to reveal coder image
 	$('.coder-wrapper').on( "mouseenter", function() {
 		$(this).closest('.split-wrapper').find('.divider-bar').css({
 			left: '25%',
@@ -16,6 +17,7 @@ jQuery(document).ready( function($){
 			transition: 'all .3s'
 		});
 	});
+	// Keep coder image displayed unless cursor enters "left-side" container, then slide back
 	$('.hero-textwrapper').on( "mouseenter", function() {
 		$(this).closest('.split-wrapper').find('.divider-bar').css({
 			left: '75%',
@@ -31,15 +33,32 @@ jQuery(document).ready( function($){
 		});
 	});
 
+	/*-----Navigation Bar Scroll View-----*/
+	$(window).scroll( function() {
+		/* If distance from current element to top of page is greater than
+		* distance from '#about' page to top of page minus banner height */
+		if ( $(this).scrollTop() > ($('#about').offset().top - $('.page-nav').height()) ){
+			$('.page-nav').fadeIn(300);
+		} else {
+			$('.page-nav').fadeOut(300);
+		}
+	});
+
 	/*-----Active Link-----*/
+	$('.hero-btn').click( function(){
+		$('.page-nav ul li.active').removeClass('active');
+		$('.page-nav ul li.start').addClass('active');
+	});
 	$('.page-nav ul li').click( function(){
 		$('.page-nav ul li.active').removeClass('active');
 		$(this).addClass('active');
 	});
+
 	/*-----Scroll Navigation-----*/
 	$('a').on('click', function() {
 		var location = (this).getAttribute("href");
 		$('html body').animate({'scrollTop':$(location).offset().top}, 500);
 		event.preventDefault();
 	});
+
 });
